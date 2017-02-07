73718204db8b0cb25b25fdae7b1e953bc300b6ef
Ralf Saenen <ralf.saenen@gmail.com>
2017-02-07 19:03:57 +0100

    apply gitignore

--- application/fe/src/environments/environment.dev.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.environment = {
-    production: false,
-    baseUrl: 'http://ng2-sf3.dev/api/'
-};
-//# sourceMappingURL=environment.dev.js.map
--- application/fe/src/environments/environment.prod.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.environment = {
-    production: true,
-    baseUrl: 'http://ng2-sf3.dev/api'
-};
-//# sourceMappingURL=environment.prod.js.map
